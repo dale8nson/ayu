aecca72941a27368250ed23e121c08f67a7f14f2
Dale Hutchinson <daletristanhutchinson@gmail.com>
2025-03-05 10:03:56 +1100

    fix: update Media and Card components for improved layout and responsiveness

--- src/components/media.rs
+++ src/components/media.rs
@@ -3,7 +3,7 @@ use leptos::prelude::*;
 #[component]
 pub fn Media(#[prop(optional)] class:String) -> impl IntoView {
     view! {
-        <img src="/assets/placeholder-image Background Removed.png" class=format!("min-w-full h-1/2 object-cover bg-transparent {}", class) />
+        <img width="360" height="188" src="/assets/placeholder-image Background Removed.png" class=format!("min-w-full h-1/2 object-fit bg-transparent {}", class) />
 
       }
 }
