e5152faa6926237fbb27814bec3918efa5988f3b
Dale Hutchinson <daletristanhutchinson@gmail.com>
2025-03-05 08:15:03 +1100

    fix: update Media component to use min-w-full for better responsiveness

--- src/components/media.rs
+++ src/components/media.rs
@@ -3,7 +3,7 @@ use leptos::prelude::*;
 #[component]
 pub fn Media(#[prop(optional)] class:String) -> impl IntoView {
     view! {
-        <img src="/assets/placeholder-image Background Removed.png" class=format!("w-full h-1/2 object-cover bg-transparent {}", class) />
+        <img src="/assets/placeholder-image Background Removed.png" class=format!("min-w-full h-1/2 object-cover bg-transparent {}", class) />
 
       }
 }
